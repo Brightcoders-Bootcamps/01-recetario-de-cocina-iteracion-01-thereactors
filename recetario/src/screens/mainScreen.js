3ede3c0a5d0f226e5bac7e6bab72861f7bb6fbdc
OwenRamire <owenomar_ramirez@ucol.mx>
2020-07-29 19:59:50 -0500

    Import card components

--- recetario/src/screens/mainScreen.js
+++ recetario/src/screens/mainScreen.js
@@ -1,49 +1,105 @@
 import React from 'react';
-import {SafeAreaView, ScrollView,View, Text, TextInput, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 import {Colors} from '../themes/colors';
+import Trending from '../components/trendingCard';
+import Recent from '../components/recentCard';
 
 export default function mainScreen() {
-    return (
-        <SafeAreaView style={styles.mainContainer}>
-            <ScrollView>
-                <View style={{styles.containerSearch}}>
-                    <View>
-                        <FontAwesomeIcons name='search' size={30} color={Colors.white}/><TextInput style={{styles.inputSearch}} placeholder='What do you want to eat?'/> <FontAwesomeIcons name='microphone' size={30} color={Colors.white}/>
-                    </View>
-                </View>
-                <View style={{styles.containerTrending}}>
-
-                </View>
-                <View style={{styles.containerRecent}}>
-                    
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
-} 
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <ScrollView>
+        {/* Container Search Input */}
+        <View style={styles.containerSearch}>
+          <FontAwesomeIcons
+            name="search"
+            size={20}
+            color="white"
+            style={styles.searchIcon}
+          />
+          <TextInput
+            style={styles.inputSearch}
+            placeholderTextColor={Colors.white}
+            placeholder="What do you want to eat?"
+          />
+          <FontAwesomeIcons
+            name="microphone"
+            size={20}
+            color="white"
+            style={styles.microIcon}
+          />
+        </View>
+        {/* Container Trending Component */}
+        <Text style={styles.Trending}>TRENDING</Text>
+        <Trending></Trending>
+        {/* Container Recents Component */}
+        <Text style={styles.Recent}>RECENT</Text>
+        <Recent/>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
-    mainContainer: {  //this is the first containes
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.backgroundScreen,
-    },
-    containerSearch: { //this is a containes
-        flex: 1,
-        backgroundColor: 'red',
-    },
-    inputSearch: {
-        color: Colors.white,
-    },
-    containerTrending: { //this is a containes
-        flex: 2,
-        backgroundColor: 'white',
-    },
-    containerRecent: {  //this is a containes
-        flex: 3,
-        backgroundColor: 'yellow',
-    },
+  mainContainer: {
+    //this is the first containes
+    flex: 1,
 
+    backgroundColor: Colors.backgroundScreen,
+  },
+
+  containerSearch: {
+    //
+    // width: '90%',
+    marginTop: 20,
+    flex: 0.08,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    backgroundColor: Colors.backgroundSearch,
+    marginHorizontal: 25,
+  },
+  inputSearch: {
+    width: 280,
+    marginHorizontal: 5,
+    color: Colors.white,
+  },
+  // searchIcon: {
+  //   marginLeft: 20,
+  // },
+  // microIcon: {
+  //   marginRight: 20,
+  // },
+  containerTrending: {
+    //this is a containes
+    marginTop: 20,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+  },
+  Trending: {
+    color: Colors.title,
+    fontSize: 24,
+    marginTop: 25,
+    marginBottom: 10,
+  },
+  containerRecent: {
+    //this is a containes
+    marginTop: 20,
+    flexDirection: 'row',
+    backgroundColor: 'yellow',
+  },
+  Recent: {
+    color: Colors.title,
+    fontSize: 24,
+    marginTop: 25,
+    marginBottom: 10,
+  },
 });
