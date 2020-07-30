13385da1311cdb539e417d1c2997a8a017213ce8
OwenRamire <owenomar_ramirez@ucol.mx>
2020-07-29 19:56:54 -0500

    Change starting screen

--- recetario/index.js
+++ recetario/index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
+import MainScreen from './src/screens/mainScreen';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MainScreen);
